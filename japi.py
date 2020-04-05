844b80517b41a11b9ab6876e3c3367d1adfb0cef
Willie <wschauml@asu.edu>
2020-04-04 18:37:44 -0700

    add files

---
+++ japi.py
@@ -0,0 +1,39 @@
+import urllib.request
+
+import json
+
+
+API_KEY = '7LECVJHIWTPL9Z32'
+
+def getStockData(symbol):
+        baseURL = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='
+        ending = '&apikey=' + API_KEY
+
+        fullURL = baseURL + symbol + ending
+        print()
+        print('Sending URL:', fullURL)
+
+        connection = urllib.request.urlopen(fullURL)
+
+        responseString = connection.read().decode()
+
+
+        #print('Response is: ', responseString)
+        d = json.loads(responseString)
+
+        dic = {}
+
+        
+        for item in d['Global Quote']:
+            ditem = item.split('. ')
+            dic[ditem[1]]=d['Global Quote'][item]
+
+        print("The current price of ", dic["symbol"], " is: ", dic["price"])
+        
+
+quit = {}
+while quit != 'quit':
+        stockSymbol = input('Enter a stock symbol: ')
+        getStockData(stockSymbol)
+
+        quit = input('To quit type quit or to look up another stock press any key and enter: ')
